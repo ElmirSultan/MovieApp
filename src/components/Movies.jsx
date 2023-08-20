b1524d29ea75615dda665b83aab760eccfd1803c
ElmirSultan <elmirsultann@gmail.com>
2023-08-20 15:13:48 +0400

    updated successfully

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -9,7 +9,7 @@ const Movies = ({dt}) => {
         <img src={`https://image.tmdb.org/t/p/original/${dt?.backdrop_path || dt?.poster_path}`} className='h-60 object-contain'/>
         <div>
             <p className='text-2xl font-bold'>{dt?.original_title}</p>
-            <p className='font-bold text-xl'>IMDB: {dt?.vote_average}</p>
+            <p className='font-bold text-xl'>IMDB: {dt?.vote_average.toFixed(1)}</p>
         </div>
     </div>
   )
